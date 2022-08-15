bfca3041721cd4e6e3c16c28c132944ac9bf3ecd
Sam Fernandes <samwallflowergirl@gmail.com>
2022-08-15 16:46:28 -0300

    login component

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
         <Header />
         <Routes>
           <Route path='/' element={<Home />}></Route>
-          <Route path='/login' element={<Login />}></Route>
+          <Route path='/login/*' element={<Login />}></Route>
         </Routes>
         <Footer />
       </BrowserRouter>
--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,8 +1,20 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import LoginForm from './LoginForm';
+import LoginCreate from './LoginCreate';
+import LoginPasswordLost from './LoginPasswordLost';
+import LoginPasswordReset from './LoginPasswordReset';
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <div>
+      <Routes>
+        <Route path="/" element={<LoginForm />} />
+        <Route path="create" element={<LoginCreate />} />
+        <Route path="passwordlost" element={<LoginPasswordLost />}/>
+        <Route path="passwordreset" element={<LoginPasswordReset />}/>
+      </Routes>
+    </div>
   )
 }
 
