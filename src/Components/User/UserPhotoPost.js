a1b3c2e7e9d701b6c41b81e474c178dd70af9198
Sam Fernandes <samwallflowergirl@gmail.com>
2022-09-23 21:53:19 -0300

    photo's feed is working photo's modal is processing

--- src/Components/User/UserPhotoPost.js
+++ src/Components/User/UserPhotoPost.js
@@ -1,10 +1,12 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import styles from './UserPhotoPost.module.css';
 import Input from '../Form/Input';
 import Button from '../Form/Button';
+import Error from '../Helper/Error';
 import useForm from '../../Hooks/useForm';
 import useFetch from '../../Hooks/useFetch';
 import { PHOTO_POST } from '../../api';
+import { useNavigate } from 'react-router-dom';
 
 const UserPhotoPost = () => {
   const name = useForm();
@@ -12,22 +14,28 @@ const UserPhotoPost = () => {
   const age = useForm('number');
   const [img, setImg] = useState({});
   const { data, loading, error, request } = useFetch();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (data) navigate('/account');
+  }, [data, navigate]);
 
   function handleSubmit(event) {
     event.preventDefault();
     const formData = new FormData();
     formData.append('img', img.raw);
-    formData.append('name', name.value);
-    formData.append('weight', weight.value);
-    formData.append('age', age.value);
+    formData.append('nome', name.value);
+    formData.append('peso', weight.value);
+    formData.append('idade', age.value);
 
     const token = window.localStorage.getItem('token');
     const { url, options } = PHOTO_POST(formData, token);
-    request(url, options);
+    request(url, options).then(res => console.log(res));
   }
 
   function handleImgChange({ target }) {
     setImg({
+      preview: URL.createObjectURL(target.files[0]),
       raw: target.files[0],
     });
   }
@@ -38,9 +46,13 @@ const UserPhotoPost = () => {
         <Input label="Name" type="text" id="name" {...name} />
         <Input label="Weight" type="number" id="weight" {...weight} />
         <Input label="Age" type="number" id="age" {...age} />
-        <input type="file" name="img" id="img" onChange={handleImgChange} />
-        <Button>Sent</Button>
+        <input className={styles.file} type="file" name="img" id="img" onChange={handleImgChange} />
+        {loading ? <Button disabled>Senting...</Button> : <Button>Sent</Button>}
+        <Error error={error} />
       </form>
+      <div>
+        {img.preview && <div className={styles.preview} style={{ backgroundImage: `url('${img.preview}')` }}></div>}
+      </div>
     </section>
   )
 }
