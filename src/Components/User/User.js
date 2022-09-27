925362f96f71ed09b0533d87bb8f1e95dca5d9c0
Sam Fernandes <samwallflowergirl@gmail.com>
2022-09-27 18:04:07 -0300

    scroll infinite is working

--- src/Components/User/User.js
+++ src/Components/User/User.js
@@ -1,16 +1,19 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import UserHeader from './UserHeader';
 import UserPhotoPost from './UserPhotoPost';
 import UserStats from './UserStats';
 import Feed from '../Feed/Feed';
 import { Routes, Route } from 'react-router-dom';
+import { UserContext } from '../../UserContext';
 
 const User = () => {
+  const { data } = useContext(UserContext);
+
   return (
     <section className="container">
       <UserHeader />
       <Routes>
-        <Route path="/" element={<Feed />} />
+        <Route path="/" element={<Feed user={data.id} />} />
         <Route path="post" element={<UserPhotoPost />} />
         <Route path="statistics" element={<UserStats />} />
       </Routes>
