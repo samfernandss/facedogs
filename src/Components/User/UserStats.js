92d7958e4b3d62f72917c7219d6a6d9679a861d0
Sam Fernandes <samwallflowergirl@gmail.com>
2022-09-29 21:41:43 -0300

    using victory library

--- src/Components/User/UserStats.js
+++ src/Components/User/UserStats.js
@@ -1,9 +1,30 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import useFetch from '../../Hooks/useFetch';
+import Loading from '../Helper/Loading';
+import Error from '../Helper/Error';
+import { STATS_GET } from '../../api';
+import UserStatsGraphs from './UserStatsGraphs';
 
 const UserStats = () => {
-  return (
-    <div>UserStats</div>
-  )
+  const { data, error, loading, request } = useFetch();
+
+  useEffect(() => {
+    async function getData() {
+      const { url, options } = STATS_GET();
+      await request(url, options);
+    }
+    getData();
+  }, [request]);
+
+  if (loading) return <Loading />;
+  if (error) return <Error error={error} />;
+  if (data)
+    return (
+      <div>
+        <UserStatsGraphs data={data} />
+      </div>
+    );
+  else return null;
 }
 
 export default UserStats;
